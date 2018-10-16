b87a4bbf0c735bd2a35eaf33873e9b85d95463bc
Mazda <ol.kyzy@gmail.com>
2018-10-16 15:17:47 +0300

    last commit

---
+++ GraficsInAdo/GraficsInAdo/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace GraficsInAdo
+{
+    class Model
+    {
+        public Model()
+        {
+
+        }
+        
+        public byte[] CreateCopy(string imagepath)
+        {
+            Image img = new Bitmap(imagepath);
+
+            int maxWidth = 300;
+            int maxHeight = 300;
+
+            double ratioX = (double)maxWidth / img.Width;
+            double ratioY = (double)maxHeight / img.Height;
+
+            double ratio = Math.Min(ratioX,ratioY);
+
+            int newWidth = (int)(img.Width * ratio);
+            int newHeight = (int)(img.Height * ratio);
+
+            Image newim = new Bitmap(newWidth,newHeight);
+
+
+            Graphics g = Graphics.FromImage(newim);
+
+            g.DrawImage(img,0,0,newWidth,newHeight);
+
+            MemoryStream ms = new MemoryStream();
+
+            newim.Save(ms,ImageFormat.Jpeg);
+            ms.Flush();
+            ms.Seek(0,SeekOrigin.Begin);
+
+            BinaryReader reader = new BinaryReader(ms);
+            return reader.ReadBytes((int)ms.Length);
+        }
+    }
+}
---
+++ GraficsInAdo/GraficsInAdo/ViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Configuration;
+using System.Windows;
+
+namespace GraficsInAdo
+{
+    class ViewModel:BindableBase 
+    {
+        #region Sql
+        private SqlConnection conn;
+        private SqlDataAdapter adapter ;
+        private DataSet dataset = new DataSet();
+        #endregion  
+
+        #region ViewMovel
+        public ObservableCollection<TabItem> TabItems { get; set; }
+        public DelegateCommand<string> Execute { get; set; }
+        #endregion
+
+        public ViewModel()
+        {
+            TabItems = new ObservableCollection<TabItem>();
+            Task.Run(()=>
+            {
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Lib"].ConnectionString);
+                Execute = new DelegateCommand<string>(_Execute);
+                adapter = new SqlDataAdapter("", conn);
+            });
+        }
+
+        private void _Execute(string com)
+        {
+            //lol
+            dataset.Tables.Clear();
+            TabItems.Clear();
+
+            adapter.SelectCommand.CommandText = com;
+            adapter.Fill(dataset);
+
+            for(int i = 0; i < dataset.Tables.Count; ++i)
+            {
+                TabItem item = new TabItem();
+                item.Header = i.ToString();
+
+                DataGrid view = new DataGrid();
+                view.ItemsSource = dataset.Tables[i].DefaultView;
+
+                item.Content = view;
+
+                TabItems.Add(item);
+            }
+        }
+    }
+}
