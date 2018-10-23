8870934f4dfcdf36892038a42564a5922048d532
Mazda <ol.kyzy@gmail.com>
2018-10-23 14:32:47 +0300

    EntityDemo

---
+++ EntityDemo/EntityDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDemo
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            using (UserContext context = new UserContext())
+            {
+                context.Users.Add(new User { name = "Oleg", age = 18 });
+                context.Users.Add(new User { name = "Test", age = 20 });
+                context.SaveChanges();
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EntityDemo/EntityDemo/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDemo
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+
+    class UserContext : DbContext
+    {
+        public UserContext() : base("EntityDemo") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
